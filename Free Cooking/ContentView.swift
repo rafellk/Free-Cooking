c0e0a68009a03789eefb86825eaaae694e64f0d6
Rafa Lucena <rafael.lucena@eero.com>
2021-05-17 14:06:19 -0300

    Initial Commit

---
+++ Free Cooking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Free Cooking
+//
+//  Created by Goncalves, Rafael on 17/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Free Cooking/Free_CookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Free_CookingApp.swift
+//  Free Cooking
+//
+//  Created by Goncalves, Rafael on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Free_CookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
